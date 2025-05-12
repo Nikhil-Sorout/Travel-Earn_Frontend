a18aae546db1c7f97d0f24d82acb4469a6c4c2d7
Nikhil <nikhilsorout6@gmail.com>
2025-05-12 14:13:03 +0530

    http to https

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://65.2.122.144:5002', // Replace with your actual base URL
+  baseURL: 'https//65.2.122.144:5002', // Replace with your actual base URL
   headers: {
     'Content-Type': 'application/json',
   },
