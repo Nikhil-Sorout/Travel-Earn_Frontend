871fdf9f7a72184dc7003b39a77eabf4884b5a05
Nikhil <nikhilsorout6@gmail.com>
2025-05-12 16:53:07 +0530

    -

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://travelandearn.host', // Replace with your actual base URL
+  baseURL: 'https://65.2.122.144',  // Use the actual IP address of your server
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 export default api;
