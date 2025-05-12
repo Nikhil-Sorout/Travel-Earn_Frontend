3f531423abe2e0fc621bd4179764b4078729ee59
Nikhil <nikhilsorout6@gmail.com>
2025-05-12 14:06:25 +0530

    deployed

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://travel-earn-backend.onrender.com', // Replace with your backend base URL
+  baseURL: 'http://65.2.122.144:5002', // Replace with your actual base URL
   headers: {
     'Content-Type': 'application/json',
   },
