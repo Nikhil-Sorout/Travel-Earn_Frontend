9738f2b910bc267a44700540c96621e005b3cfda
Nikhil <nikhilsorout6@gmail.com>
2025-05-12 16:07:31 +0530

    Https allowed

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://65.2.122.144:5002', // Replace with your actual base URL
+  baseURL: 'https://65.2.122.144', // Replace with your actual base URL
   headers: {
     'Content-Type': 'application/json',
   },
