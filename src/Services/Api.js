4b510d9a2a8152edcefae1043999a81e496fd6c1
Tushar Daiya <tushardaiya@outlook.com>
2025-08-25 18:26:54 +0530

    update api url

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002',  // Use the actual IP address of your server  // Use the actual IP address of your server
+  baseURL: 'https://travel.timestringssystem.com',  // Use the actual IP address of your server  // Use the actual IP address of your server
   // baseURL: 'http://192.168.65.0:5002',
   headers: {
     'Content-Type': 'application/json',
